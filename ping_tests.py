2eea8e627cc98b977f4a176b8586ed0575e279e1
Independent3 <Nickvasilakopoulos@rocketmail.com>
2026-02-03 22:18:35 +0200

    Wrap main logic in main() function
    -Encapsulated the program logic inside a main() function
    -Added if name == "main" to allow safe imports
    -No changes to ping functionality or counters

--- ping_tests.py
+++ ping_tests.py
@@ -43,45 +43,48 @@ def ping(host , ping_count):
            "timeout": timeout_count,
            "avg_latency": average_latency,
            "message": message}
-try:
-    print("How many times you want to ping each host?")
-    ping_count = int(input())
-    if ping_count > 0:
-        print("Give the name of the hosts you want to ping separated with space or comma (Duplicates will be ignored)")
+def main():
+    try:
+        print("How many times you want to ping each host?")
+        ping_count = int(input())
+        if ping_count > 0:
+            print("Give the name of the hosts you want to ping separated by space or comma (Duplicates will be ignored)")
 
-        hosts = set()
-        failure = 0
-        success = 0
-        timeouts = 0
+            hosts = set()
+            failure = 0
+            success = 0
+            timeouts = 0
 
-        for host in (input().replace("," , "").split()):  #first i use replace to replace any comma with space and then split with no arguments handles messy spacing
-            if host:  #defensive programming
-                if host[0].isalpha() or host[0].isdigit():
-                    if host not in hosts:  #not really needed since i use a set() for this
-                        hosts.add(host)
-                else:
-                    print(f'Wrong input. Host name should start with A-Z or 0-9 for, {host}')
-                    failure += 1
-        if hosts:
-            for host in hosts:
-                result = ping(host,ping_count)
-                if result['avg_latency'] != None:
-                    print(f"{host}: {result['avg_latency']} ms")
-                if result['timeout'] > 0 and result['success'] == 0:
-                    print(f"{host}: Host is reachable but no responses received, Timeouts: {result['timeout']}")
-                if result['message']:
-                    print(result['message'])
-                success += result['success']
-                failure += result['failure']
-                timeouts += result['timeout']
+            for host in (input().replace("," , "").split()):  #first i use replace to replace any comma with space and then split with no arguments handles messy spacing
+                if host:  #defensive programming
+                    if host[0].isalpha() or host[0].isdigit():
+                        if host not in hosts:  #not really needed since i use a set() for this
+                            hosts.add(host)
+                    else:
+                        print(f'Wrong input. Host name should start with A-Z or 0-9 for, {host}')
+                        failure += 1
+            if hosts:
+                for host in hosts:
+                    result = ping(host,ping_count)
+                    if result['avg_latency'] != None:
+                        print(f"{host}: {result['avg_latency']} ms")
+                    if result['timeout'] > 0 and result['success'] == 0:
+                        print(f"{host}: Host is reachable but no responses received, Timeouts: {result['timeout']}")
+                    if result['message']:
+                        print(result['message'])
+                    success += result['success']
+                    failure += result['failure']
+                    timeouts += result['timeout']
+            else:
+                print("No hosts found")
+
+            total_attempted_pings = success + failure + timeouts
+
+            print(f'Total number of attempted pings: {total_attempted_pings},\nSuccessful pings: {success} \nFailed attempts: {failure}\nTimeouts: {timeouts}')
         else:
-            print("No hosts found")
-
-        total_attempted_pings = success + failure + timeouts
-
-        print(f'Total number of attempted pings: {total_attempted_pings},\nSuccessful pings: {success} \nFailed attempts: {failure}\nTimeouts: {timeouts}')
-    else:
-        print("Number should be a positive")
-except ValueError:
-    print("You must enter a valid number")
+            print("Number should be a positive")
+    except ValueError:
+        print("You must enter a valid number")
+if __name__ == "__main__": #making importing safe/with no troubles
+    main()
 
